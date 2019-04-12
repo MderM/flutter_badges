8f30f2d84a2b2aba7b82d94184097dc5dc82b364
MderM <maikmdermmewes@gmail.com>
2019-04-12 16:42:58 +0200

    + adjusted some visuals for own needs + added badge menu icon button

--- lib/src/badge_icon_button.dart
+++ lib/src/badge_icon_button.dart
@@ -125,3 +125,137 @@ class BadgeIconButtonState extends State<BadgeIconButton>
     _animationController.forward();
   }
 }
+
+class BadgeMenuIconButton<T> extends StatefulWidget {
+  final int itemCount;
+  final Color badgeColor;
+  final Icon icon;
+  final bool hideZeroCount;
+  final bool toAnimate;
+  final BadgePosition position;
+  final BadgeShape shape;
+  final TextStyle textStyle;
+  final EdgeInsets padding;
+  final Duration animationDuration;
+  final PopupMenuItemBuilder<T> itemBuilder;
+  final PopupMenuItemSelected<T> onSelected;
+  final PopupMenuCanceled popupMenuCanceled;
+
+  BadgeMenuIconButton(
+      {Key key,
+        @required this.itemCount,
+        @required this.icon,
+        @required this.itemBuilder,
+        this.onSelected,
+        this.popupMenuCanceled,
+        this.hideZeroCount: true,
+        this.badgeColor: Colors.red,
+        this.toAnimate: true,
+        this.position: BadgePosition.topRight,
+        this.shape: BadgeShape.circle,
+        this.textStyle: const TextStyle(
+          fontSize: 13.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        this.padding: const EdgeInsets.all(5.0),
+        this.animationDuration: const Duration(milliseconds: 500)})
+      : assert(itemCount >= 0),
+        assert(badgeColor != null),
+        super(key: key);
+
+  @override
+  BadgeMenuIconButtonState createState() {
+    return BadgeMenuIconButtonState();
+  }
+}
+
+class BadgeMenuIconButtonState extends State<BadgeMenuIconButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+
+  final Tween<Offset> _badgePositionTween = Tween(
+    begin: const Offset(-0.5, 0.9),
+    end: const Offset(0.0, 0.0),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.hideZeroCount && widget.itemCount == 0) {
+      return PopupMenuButton(
+        icon: widget.icon,
+        onSelected: widget.onSelected,
+        onCanceled: widget.popupMenuCanceled,
+        itemBuilder: widget.itemBuilder,
+      );
+    }
+
+    return PopupMenuButton(
+        onSelected: widget.onSelected,
+        onCanceled: widget.popupMenuCanceled,
+        itemBuilder: widget.itemBuilder,
+        child: Stack(
+          fit: StackFit.passthrough,
+          overflow: Overflow.visible,
+          children: [
+            Padding(
+              child: widget.icon,
+              padding: EdgeInsets.only(right: 8),
+            ),
+            Positioned(
+              top: -2.0, left: 12.0,
+              child: widget.toAnimate
+                  ? SlideTransition(
+                  position: _badgePositionTween.animate(_animation),
+                  child: _getBadge())
+                  : _getBadge(),
+            ),
+          ],
+        ),);
+  }
+
+  Widget _getBadge() {
+    return
+      Material(
+          type: widget.shape == BadgeShape.circle
+              ? MaterialType.circle
+              : MaterialType.card,
+          elevation: 2.0,
+          color: widget.badgeColor,
+          child: Padding(
+            padding: widget.padding,
+            child: Text(
+              widget.itemCount.toString(),
+              style: widget.textStyle,
+            ),
+          ));
+  }
+
+  @override
+  void didUpdateWidget(BadgeMenuIconButton oldWidget) {
+    if (widget.itemCount != oldWidget.itemCount) {
+      _animationController.reset();
+      _animationController.forward();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: widget.animationDuration,
+      vsync: this,
+    );
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.elasticOut);
+    _animationController.forward();
+  }
+}
